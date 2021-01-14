e651567eef4a036037e4c9063f47ce2b852d4e17
Sukru Oytun Coban <cobans17@itu.edu.tr>
2021-01-14 06:01:17 +0300

    JS File added for toggling forms
    .js file is added for toggling forms to display onClick.

---
+++ insert_entry.js
@@ -0,0 +1,10 @@
+function toggle_teacher_add() {
+    document.getElementById('').style.display='block';
+    document.getElementById('').style.display='none';
+}
+
+function toggle_team_add() {
+    document.getElementById('').style.display='block';
+    document.getElementById('').style.display='none';
+}
+
